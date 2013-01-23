5501a0821560b2b28f109bdcf961d94c299349fd
Matt Miller <matt@facultycreative.com>
2013-01-23 12:30:13 -0500

    - Updated server so that all requests (non static) are display the index page. - Removed the error function in server

--- src/server/lib/errors/index.js
+++ src/server/lib/errors/index.js
@@ -3,10 +3,5 @@ var path    = require('path');
 var app     = module.exports = express();
 
 app.get('*', function(req, res) {
-  res.redirect('/#' + req.url);
-  //res.sendfile(path.join(__dirname, '../../../../dist/index.html'));
-});
-
-app.use(function(err, req, res, next) {
-  res.redirect('/#' + req.url + (req.url.match(/\?/) ? '&' : '?') + 'err=500');
+  res.sendfile(path.join(__dirname, '../../../../dist/index.html'));
 });
